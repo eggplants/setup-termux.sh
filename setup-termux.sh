a6f13a790335f70711193d8b4d955c67f7301777
haruna <w10776e8w@yahoo.co.jp>
2024-11-14 06:01:44 +0900

    Update setup-termux.sh

--- setup-termux.sh
+++ setup-termux.sh
@@ -72,7 +72,7 @@ A
 
 # git
 [[ -f ~/.gitconfig ]] || {
-  gh auth login
+  gh auth login -p https -h gitHub.com -w <<<y
   git config --global commit.gpgsign true
   git config --global core.editor nano
   git config --global gpg.program "$(which gpg)"
